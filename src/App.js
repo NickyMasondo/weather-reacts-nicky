032c0a433fc32aa216a9ebe808523e5de914fdc2
NickyMasondo <nickymasondo@gmail.com>
2024-07-15 21:22:27 +0200

    new changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import React from "react";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Header />
-        <Main />
-        <Footer />
+        <Weather defaultCity="London" />
+        <footer>
+          <span> This project was coded by </span>
+          <a
+            href="https://github.com/elizabethmoon37"
+            target="blank"
+            rel="noreferrer"
+          >
+            Beth Moon
+          </a>
+          <span> and is </span>
+
+          <a
+            href="https://github.com/elizabethmoon37/weather-react"
+            target="blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
       </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,72 @@
-import react from "react";
+import React, { useState } from "react";
 
-export default function Weather() {
-  return <div className="App"></div>;
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      iconUrl: response.data.condition.icon_url,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  function search() {
+    const apiKey = "6975040fc193feta4f47d8af4o55baa9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast data={weatherData.city} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
